206134fa74b3d27ed4dc74ae6567cab28dc52df3
Akash Prajapati <akashprajapati1849@gmail.com>
2024-01-16 17:57:06 +0530

    updated URL for production

--- jokeAPI.js
+++ jokeAPI.js
@@ -1,5 +1,5 @@
 async function fetchAPI() {
-    let response = await fetch("http://127.0.0.1:8080/joke-api/allJokes");
+    let response = await fetch("https://joke-api-production-e11e.up.railway.app/joke-api/allJokes");
     console.log(response);
     console.log(response.status);
     console.log(response.ok);

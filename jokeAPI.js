0040780f1586e5ab6d314f36b06152d145cbe002
Akash Prajapati <akashprajapati1849@gmail.com>
2024-01-17 00:42:45 +0530

    moved random joke logic to server side

--- jokeAPI.js
+++ jokeAPI.js
@@ -1,5 +1,6 @@
 async function fetchAPI() {
-    let response = await fetch("https://joke-api-production-e11e.up.railway.app/joke-api/allJokes");
+    let response = await fetch("https://joke-api-production-e11e.up.railway.app/joke-api/getOneJoke");
+    // let response = await fetch("http://127.0.0.1:8080/joke-api/getOneJoke");
     console.log(response);
     console.log(response.status);
     console.log(response.ok);
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 import { jokeFromAPI } from './jokeAPI.js';
-console.log(typeof jokeFromAPI);
+/* console.log(typeof jokeFromAPI);
 console.log("jokem", jokeFromAPI);
-const random = Math.floor(Math.random()*jokeFromAPI.length);
+const random = Math.floor(Math.random()*jokeFromAPI.length); */
 // console.log(jokeFromAPI.length);
 // console.log(random);
 let paragraph = document.body.children[1].firstElementChild.firstElementChild;
 // console.log(paragraph);
 // console.log(jokeFromAPI[random])
-let lines = jokeFromAPI[random].split(/\n/g);
+let lines = jokeFromAPI[0].split(/\n/g);
 
 // console.log(jokeFromAPI[54])
 // console.log(lines);
